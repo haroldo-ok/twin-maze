dc9c7056c29f34347aa1e9eeb7b5e4e301418c02
Haroldo de Oliveira Pinheiro <haroldoop@gmail.com>
2016-02-29 22:24:17 -0300

    Corridor prototype

--- maze.c
+++ maze.c
@@ -6,8 +6,58 @@
 #include "PSGlib/src/PSGlib.h"
 #include "gfx.h"
 
+#define WINDOW_WIDTH 14
+#define WINDOW_HEIGHT 14
+#define WINDOW_CENTER_X (WINDOW_WIDTH >> 1)
+
+void load_palette() {
+	unsigned char i;
+
+	for (i=0;i<16;i++) {
+    SMS_setBGPaletteColor(i,0x00);    // black
+	}
+  SMS_setBGPaletteColor(01,0x3f);     // white
+}
+
+void load_font(void) {
+	unsigned char i, j;
+	unsigned char buffer[32], *o, *d;
+
+	o = font_fnt;
+	for (i = 0; i != 96; i++) {
+		d = buffer;
+		for (j = 0; j != 8; j++) {
+			*d = *o; d++;	o++;
+			*d = 0;	d++;
+			*d = 0;	d++;
+			*d = 0;	d++;
+		}
+		SMS_loadTiles(buffer, i, 32);
+	}
+}
+
+void draw_char(unsigned char x, unsigned char y, char c) {
+  SMS_setTileatXY(x, y, c - 32);
+}
+
+void draw_window(unsigned char x, unsigned char y) {
+	unsigned char i;
+
+	for (i = 0; i != WINDOW_CENTER_X; i++) {
+		draw_char(x + i, y + i, '\\');
+		draw_char(x + i, y + WINDOW_HEIGHT - i, '/');
+		draw_char(x + WINDOW_WIDTH - i, y + i, '/');
+		draw_char(x + WINDOW_HEIGHT - i, y + WINDOW_HEIGHT - i, '\\');
+	}
+}
 
 void main(void) {
+	load_palette();
+	load_font();
+	SMS_displayOn();
+
+	draw_window(1, 1);
+	draw_window(17, 1);
 
 	while (true) {}
 
