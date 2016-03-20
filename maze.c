5b32afcaef435c342251610f2c7b352b67a7159c
Haroldo de Oliveira Pinheiro <haroldoop@gmail.com>
2016-03-20 14:05:56 -0300

    Updated release date

--- maze.c
+++ maze.c
@@ -417,6 +417,6 @@ void main(void) {
 }
 
 SMS_EMBED_SEGA_ROM_HEADER(9999,0); // code 9999 hopefully free, here this means 'homebrew'
-SMS_EMBED_SDSC_HEADER(0,1, 2016,2,29, "Haroldo-OK\\2016", "Twin Maze",
+SMS_EMBED_SDSC_HEADER(0,1, 2016,3,20, "Haroldo-OK\\2016", "Twin Maze",
   "A first person maze game.\n"
   "Built using devkitSMS & SMSlib - https://github.com/sverx/devkitSMS");
